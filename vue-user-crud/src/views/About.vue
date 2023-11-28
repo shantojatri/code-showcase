0406c6519f73dc8515680bc506638d5d0622b0bc
shantojatri <shanto.jatri@gmail.com>
2023-11-28 16:39:23 +0600

    Added all pages

---
+++ vue-user-crud/src/views/About.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"> 
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline text-red-500">
+      About Page
+    </h1>
+  </div>
+</template>
---
+++ vue-user-crud/src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"> 
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline text-red-500">
+      Hello world!
+    </h1>
+  </div>
+</template>
