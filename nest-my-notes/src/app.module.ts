4a8c2328819df216701d6f96d9a23d8461b54f9e
shantojatri <shanto.jatri@gmail.com>
2023-12-01 12:34:42 +0600

    added some packeges and autha nad notes module

--- nest-my-notes/src/app.module.ts
+++ nest-my-notes/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'; 
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_DB_URI),
+    AuthModule,
+    NotesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- nest-my-notes/src/main.ts
+++ nest-my-notes/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'; 
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+  await app.listen(parseInt(process.env.APP_PORT));
+  // await app.listen(3010);
 }
 bootstrap();
