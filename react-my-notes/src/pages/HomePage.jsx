bd5df4eeb5f63485728ecbcf17ab5aa3f5e71aef
shantojatri <shanto.jatri@gmail.com>
2023-12-08 01:09:41 +0600

    Update home page

--- react-my-notes/src/pages/HomePage.jsx
+++ react-my-notes/src/pages/HomePage.jsx
@@ -1,158 +1,168 @@
 const index = () => {
   return (
-    <div className="grid grid-cols-4 gap-5 my-16">
-      {/* Single Note item */}
-      <div className="bg-red-200 p-4 rounded-lg">
-        {/* Note header */}
-        <div className="flex items-center justify-between mb-2">
-          <h3 className="text-lg font-bold">Hello Dello</h3>
+    <>
+      <div class="w-full mt-12">
+        <div class="flex items-center justify-between">
+          <h2 class="text-base font-semibold text-gray-700">
+            Please login to store your note.
+          </h2>
         </div>
-
-        {/* Notes content */}
-        <p className="text-justify text-base font-normal">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
-          exercitationem dolore vero assumenda repellendus, quia eum totam
-          quaerat obcaecati repellat.
-        </p>
-        {/* Showing created at */}
-        <p className="text-md text-black mt-4">
-          Created: <span className="font-semibold">22/10/2023</span>
-        </p>
       </div>
 
-      {/* Single Note item */}
-      <div className="bg-blue-200 p-4 rounded-lg">
-        {/* Note header */}
-        <div className="flex items-center justify-between mb-2">
-          <h3 className="text-lg font-bold">Hello Dello</h3>
+      <div className="grid grid-cols-4 gap-5 my-7">
+        {/* Single Note item */}
+        <div className="bg-red-200 p-4 rounded-lg">
+          {/* Note header */}
+          <div className="flex items-center justify-between mb-2">
+            <h3 className="text-lg font-bold">Hello Dello</h3>
+          </div>
+
+          {/* Notes content */}
+          <p className="text-justify text-base font-normal">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
+            exercitationem dolore vero assumenda repellendus, quia eum totam
+            quaerat obcaecati repellat.
+          </p>
+          {/* Showing created at */}
+          <p className="text-md text-black mt-4">
+            Created: <span className="font-semibold">22/10/2023</span>
+          </p>
         </div>
 
-        {/* Notes content */}
-        <p className="text-justify text-base font-normal">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
-          exercitationem dolore vero assumenda repellendus, quia eum totam
-          quaerat obcaecati repellat.
-        </p>
-        {/* Showing created at */}
-        <p className="text-md text-black mt-4">
-          Created: <span className="font-semibold">22/10/2023</span>
-        </p>
-      </div>
+        {/* Single Note item */}
+        <div className="bg-blue-200 p-4 rounded-lg">
+          {/* Note header */}
+          <div className="flex items-center justify-between mb-2">
+            <h3 className="text-lg font-bold">Hello Dello</h3>
+          </div>
 
-      {/* Single Note item */}
-      <div className="bg-purple-200 p-4 rounded-lg">
-        {/* Note header */}
-        <div className="flex items-center justify-between mb-2">
-          <h3 className="text-lg font-bold">Hello Dello</h3>
+          {/* Notes content */}
+          <p className="text-justify text-base font-normal">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
+            exercitationem dolore vero assumenda repellendus, quia eum totam
+            quaerat obcaecati repellat.
+          </p>
+          {/* Showing created at */}
+          <p className="text-md text-black mt-4">
+            Created: <span className="font-semibold">22/10/2023</span>
+          </p>
         </div>
 
-        {/* Notes content */}
-        <p className="text-justify text-base font-normal">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
-          exercitationem dolore vero assumenda repellendus, quia eum totam
-          quaerat obcaecati repellat.
-        </p>
-        {/* Showing created at */}
-        <p className="text-md text-black mt-4">
-          Created: <span className="font-semibold">22/10/2023</span>
-        </p>
-      </div>
+        {/* Single Note item */}
+        <div className="bg-purple-200 p-4 rounded-lg">
+          {/* Note header */}
+          <div className="flex items-center justify-between mb-2">
+            <h3 className="text-lg font-bold">Hello Dello</h3>
+          </div>
 
-      {/* Single Note item */}
-      <div className="bg-cyan-200 p-4 rounded-lg">
-        {/* Note header */}
-        <div className="flex items-center justify-between mb-2">
-          <h3 className="text-lg font-bold">Hello Dello</h3>
+          {/* Notes content */}
+          <p className="text-justify text-base font-normal">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
+            exercitationem dolore vero assumenda repellendus, quia eum totam
+            quaerat obcaecati repellat.
+          </p>
+          {/* Showing created at */}
+          <p className="text-md text-black mt-4">
+            Created: <span className="font-semibold">22/10/2023</span>
+          </p>
         </div>
 
-        {/* Notes content */}
-        <p className="text-justify text-base font-normal">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
-          exercitationem dolore vero assumenda repellendus, quia eum totam
-          quaerat obcaecati repellat.
-        </p>
-        {/* Showing created at */}
-        <p className="text-md text-black mt-4">
-          Created: <span className="font-semibold">22/10/2023</span>
-        </p>
-      </div>
+        {/* Single Note item */}
+        <div className="bg-cyan-200 p-4 rounded-lg">
+          {/* Note header */}
+          <div className="flex items-center justify-between mb-2">
+            <h3 className="text-lg font-bold">Hello Dello</h3>
+          </div>
 
-      {/* Single Note item */}
-      <div className="bg-blue-200 p-4 rounded-lg">
-        {/* Note header */}
-        <div className="flex items-center justify-between mb-2">
-          <h3 className="text-lg font-bold">Hello Dello</h3>
+          {/* Notes content */}
+          <p className="text-justify text-base font-normal">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
+            exercitationem dolore vero assumenda repellendus, quia eum totam
+            quaerat obcaecati repellat.
+          </p>
+          {/* Showing created at */}
+          <p className="text-md text-black mt-4">
+            Created: <span className="font-semibold">22/10/2023</span>
+          </p>
         </div>
 
-        {/* Notes content */}
-        <p className="text-justify text-base font-normal">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
-          exercitationem dolore vero assumenda repellendus, quia eum totam
-          quaerat obcaecati repellat.
-        </p>
-        {/* Showing created at */}
-        <p className="text-md text-black mt-4">
-          Created: <span className="font-semibold">22/10/2023</span>
-        </p>
-      </div>
+        {/* Single Note item */}
+        <div className="bg-blue-200 p-4 rounded-lg">
+          {/* Note header */}
+          <div className="flex items-center justify-between mb-2">
+            <h3 className="text-lg font-bold">Hello Dello</h3>
+          </div>
 
-      {/* Single Note item */}
-      <div className="bg-purple-200 p-4 rounded-lg">
-        {/* Note header */}
-        <div className="flex items-center justify-between mb-2">
-          <h3 className="text-lg font-bold">Hello Dello</h3>
+          {/* Notes content */}
+          <p className="text-justify text-base font-normal">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
+            exercitationem dolore vero assumenda repellendus, quia eum totam
+            quaerat obcaecati repellat.
+          </p>
+          {/* Showing created at */}
+          <p className="text-md text-black mt-4">
+            Created: <span className="font-semibold">22/10/2023</span>
+          </p>
         </div>
 
-        {/* Notes content */}
-        <p className="text-justify text-base font-normal">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
-          exercitationem dolore vero assumenda repellendus, quia eum totam
-          quaerat obcaecati repellat.
-        </p>
-        {/* Showing created at */}
-        <p className="text-md text-black mt-4">
-          Created: <span className="font-semibold">22/10/2023</span>
-        </p>
-      </div>
+        {/* Single Note item */}
+        <div className="bg-purple-200 p-4 rounded-lg">
+          {/* Note header */}
+          <div className="flex items-center justify-between mb-2">
+            <h3 className="text-lg font-bold">Hello Dello</h3>
+          </div>
 
-      {/* Single Note item */}
-      <div className="bg-pink-200 p-4 rounded-lg">
-        {/* Note header */}
-        <div className="flex items-center justify-between mb-2">
-          <h3 className="text-lg font-bold">Hello Dello</h3>
+          {/* Notes content */}
+          <p className="text-justify text-base font-normal">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
+            exercitationem dolore vero assumenda repellendus, quia eum totam
+            quaerat obcaecati repellat.
+          </p>
+          {/* Showing created at */}
+          <p className="text-md text-black mt-4">
+            Created: <span className="font-semibold">22/10/2023</span>
+          </p>
         </div>
 
-        {/* Notes content */}
-        <p className="text-justify text-base font-normal">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
-          exercitationem dolore vero assumenda repellendus, quia eum totam
-          quaerat obcaecati repellat.
-        </p>
-        {/* Showing created at */}
-        <p className="text-md text-black mt-4">
-          Created: <span className="font-semibold">22/10/2023</span>
-        </p>
-      </div>
+        {/* Single Note item */}
+        <div className="bg-pink-200 p-4 rounded-lg">
+          {/* Note header */}
+          <div className="flex items-center justify-between mb-2">
+            <h3 className="text-lg font-bold">Hello Dello</h3>
+          </div>
 
-      {/* Single Note item */}
-      <div className="bg-green-200 p-4 rounded-lg">
-        {/* Note header */}
-        <div className="flex items-center justify-between mb-2">
-          <h3 className="text-lg font-bold">Hello Dello</h3>
+          {/* Notes content */}
+          <p className="text-justify text-base font-normal">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
+            exercitationem dolore vero assumenda repellendus, quia eum totam
+            quaerat obcaecati repellat.
+          </p>
+          {/* Showing created at */}
+          <p className="text-md text-black mt-4">
+            Created: <span className="font-semibold">22/10/2023</span>
+          </p>
         </div>
 
-        {/* Notes content */}
-        <p className="text-justify text-base font-normal">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
-          exercitationem dolore vero assumenda repellendus, quia eum totam
-          quaerat obcaecati repellat.
-        </p>
-        {/* Showing created at */}
-        <p className="text-md text-black mt-4">
-          Created: <span className="font-semibold">22/10/2023</span>
-        </p>
+        {/* Single Note item */}
+        <div className="bg-green-200 p-4 rounded-lg">
+          {/* Note header */}
+          <div className="flex items-center justify-between mb-2">
+            <h3 className="text-lg font-bold">Hello Dello</h3>
+          </div>
+
+          {/* Notes content */}
+          <p className="text-justify text-base font-normal">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
+            exercitationem dolore vero assumenda repellendus, quia eum totam
+            quaerat obcaecati repellat.
+          </p>
+          {/* Showing created at */}
+          <p className="text-md text-black mt-4">
+            Created: <span className="font-semibold">22/10/2023</span>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
